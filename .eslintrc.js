3b79b9fcd810dd57d5a850601abb92a526d8ae7f
Amber Holly <amber@amber.vision>
2021-10-24 16:13:23 +0100

    Add Prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,7 +7,8 @@ module.exports = {
   'extends': [
     'eslint:recommended',
     'plugin:react/recommended',
-    'plugin:react/jsx-runtime'
+    'plugin:react/jsx-runtime',
+    'prettier'
   ],
   'parserOptions': {
     'ecmaFeatures': {
