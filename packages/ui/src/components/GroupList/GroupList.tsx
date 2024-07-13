5189944c19d31c5994a0ee0c73ef1acac3070207
Amber Cronin <amber@amber.vision>
2024-07-13 01:30:24 +0100

    Sort groups alphabetically

--- packages/ui/src/components/GroupList/GroupList.tsx
+++ packages/ui/src/components/GroupList/GroupList.tsx
@@ -5,10 +5,16 @@ import { Link } from "wouter";
 import GroupCard from "../GroupCard/GroupCard";
 import { type Group } from "@starlight/types";
 
+const groupSort = (a: Group, b: Group) => {
+  if (a.friendly_name > b.friendly_name) return 1;
+  if (b.friendly_name > a.friendly_name) return -1;
+  return 0;
+};
+
 function GroupList(props: { groups: Group[]; onClick?: Function }) {
   return (
     <div>
-      {props.groups.map((group: Group) => (
+      {props.groups.sort(groupSort).map((group: Group) => (
         <Link href={`/groups/${group.id}`} key={group.id}>
           <GroupCard group={group} onClick={props.onClick} />
         </Link>
