3f28f5605afb73278074b4fd57c19e2488c9e31a
Amber Cronin <amber@amber.vision>
2024-07-18 00:44:12 +0100

    Backport device sort from development

--- src/components/DeviceList/DeviceList.js
+++ src/components/DeviceList/DeviceList.js
@@ -3,10 +3,17 @@
 import { Link } from 'wouter';
 import DeviceCard from '../DeviceCard/DeviceCard';
 
+const deviceSort = (a, b) => {
+  a.friendly_name[0] - b.friendly_name[0] ||
+    a.friendly_name.localeCompare(b.friendly_name, undefined, {
+      numeric: true,
+    });
+};
+
 function DeviceList(props) {
   return (
     <div>
-      {props.devices.map((device) => (
+      {props.devices.sort(deviceSort).map((device) => (
         <Link
           href={`/devices/${device.friendly_name}`}
           key={device.ieee_address}
