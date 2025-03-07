7b2e386da2e17d36e51928f3b15a6e3552da2922
Amber Cronin <amber@amber.vision>
2025-03-07 09:53:43 +0000

    Update package.json, tsconfig.json in prep for containerisation

---
+++ packages/ui/vite.config.d.ts
@@ -0,0 +1,5 @@
+// SPDX-FileCopyrightText: © 2025 Amber Cronin <software@amber.vision>
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+declare const _default: import("vite").UserConfig;
+export default _default;
